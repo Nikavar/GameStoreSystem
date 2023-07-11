28cf5a293c47bd26be4635e8f43b41005e2e3442
Nikoloz Varamashvili <varamashvili.nikoloz@gmail.com>
2023-07-11 15:57:03 +0400

    changes in Http Verbs

--- GameStore/Controllers/GameController.cs
+++ GameStore/Controllers/GameController.cs
@@ -26,7 +26,7 @@ namespace GameStore.Controllers
             this.mapper = mapper;
         }
 
-        [HttpGet]
+        [HttpGet("AllGame")]
         public async Task<ActionResult> GetAllAsync()
         {
             var gameList = await gameService.GetAllGamesAsync();
@@ -117,7 +117,7 @@ namespace GameStore.Controllers
         }
 
         // task 1.6
-        [HttpPut]
+        [HttpPut("AddImageToGame")]
         public async Task<ActionResult> AddImageToGameAsync([FromBody] GameModel model)
         {
             if (gameService.IsGameModelValidate(model))
@@ -129,7 +129,7 @@ namespace GameStore.Controllers
             return BadRequest();
         }
 
-        [HttpPut]
+        [HttpPut("Update")]
         public async Task<ActionResult> UpdateAsync([FromBody] GameModel model)
         {
             if (gameService.IsGameModelValidate(model))
