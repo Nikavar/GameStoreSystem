1a3b7cb1d3309433a7fafc9aa902e6e39baa8d7c
Nikoloz Varamashvili <varamashvili.nikoloz@gmail.com>
2023-07-14 15:00:51 +0400

    version 1.4

--- GameStore.Service/AccountService.cs
+++ GameStore.Service/AccountService.cs
@@ -12,6 +12,7 @@ using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
 using System.Net.Http;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -46,16 +47,55 @@ namespace GameStore.Service
             // Because email must be unique, I use it to check if the same account is already in DB or not
             var account = await accountRepository.GetManyAsync(X => X.Email.Equals(model.Email));
 
-            // If not, I create a new one!
-            if(account.FirstOrDefault() == null)
+            // if not, I create a new one!
+            if (account.FirstOrDefault() == null)
             {
-                var entity = mapper.Map<Account>(model);
-                var result = await accountRepository.RegisterAccountAsync(entity);
+                if (model.Password != null)
+                {
+                    var hashedPassword = ComputeSha256Hash(model.Password);
+                    model.Password = hashedPassword;
 
-                return result;
+                    var entity = new Account
+                    {
+                        Id = model.Id,
+                        FirstName = model.FirstName,
+                        LastName = model.LastName,
+                        Email = model.Email,
+                        Password = hashedPassword,
+                        UserName = model.UserName,
+                        AvatarImage = model.AvatarImage
+                    };
+
+                    var entity2 = mapper.Map<Account>(entity);
+
+                    var result = await accountRepository.AddAsync(entity);
+
+                    return result;
+                }
+
+                throw new NullReferenceException("Your password must not be null");
+
             }
 
             throw new Exception("This account is already exists!");
+        }
+
+        private string ComputeSha256Hash(string rawData)
+        {
+            // Create a SHA256
+            using (SHA256 sha256Hash = SHA256.Create())
+            {
+                // ComputeHash - returns byte array
+                byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(rawData));
+
+                // Convert byte array to a string
+                StringBuilder builder = new StringBuilder();
+                for (int i = 0; i < bytes.Length; i++)
+                {
+                    builder.Append(bytes[i].ToString("x2"));
+                }
+                return builder.ToString();
+            }
         }
 
         public async Task UpdateAccountAsync(Account entity)
