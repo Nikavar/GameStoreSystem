90c0ce3a70a69068291fd9b19acebc1717ede587
Nikoloz Varamashvili <varamashvili.nikoloz@gmail.com>
2023-07-14 15:02:47 +0400

    version 1.5

--- GameStore.Service/AccountService.cs
+++ GameStore.Service/AccountService.cs
@@ -23,23 +23,27 @@ namespace GameStore.Service
         private readonly IAccountRepository accountRepository;
         private readonly IUnitOfWork unitOfWork;
         private readonly IMapper mapper;
+        private readonly IConfiguration configuration;
 
-        public AccountService(IAccountRepository accountRepository, IUnitOfWork unitOfWork, IMapper mapper)
+        public AccountService(IAccountRepository accountRepository, IUnitOfWork unitOfWork, IMapper mapper, IConfiguration configuration)
         {
             this.accountRepository = accountRepository;
             this.unitOfWork = unitOfWork;
             this.mapper = mapper;
+            this.configuration = configuration;
         }
 
         public async Task<Account> LoginAccountAsync(string username, string password)
         {
-            if(string.IsNullOrEmpty(username) && string.IsNullOrEmpty(password))
+            if (string.IsNullOrEmpty(username) && string.IsNullOrEmpty(password))
             {
-                throw new NullReferenceException();
-            }  
-                
-            return await accountRepository.LoginAccountAsync(username, password);   
+                throw new Exception("username or/and password is null or empty!");
+            }
 
+            password = ComputeSha256Hash(password);
+            var entity = await accountRepository.GetManyAsync(x => x.UserName.ToLower() == username.ToLower() && x.Password == password);
+
+            return entity.FirstOrDefault();
         }
 
         public async Task<Account> RegisterAccountAsync(AccountModel model)
